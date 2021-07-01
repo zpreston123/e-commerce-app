091bb0cc5f4f5338db037d367716b93780fe833a
Zach Preston <zpdrums@comcast.net>
2021-06-30 21:41:32 -0400

    fix custom text field component

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,12 +1,22 @@
 import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
-const FormInput = ({ name, label }) => {
+const FormInput = ({ name, label, required }) => {
 	const { control } = useFormContext();
 
 	return (
 		<Grid item xs={12} sm={6}>
-			<Controller as={TextField} defaultValue="" control={control} fullWidth name={name} label={label} required />
+			<Controller
+				control={control}
+				name={name}
+				render={({ field }) => (
+					<TextField
+						fullWidth
+						label={label}
+						required
+					/>
+				)}
+			/>
 		</Grid>
 	);
 };
